93b85f5d6008554979f3a7e6fb3987ecac72d849
AndyLipo <andliporace@hotmail.com>
2026-05-21 19:54:58 -0300

    feat: add tailwind v4 theme config and app styles

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Navbar } from './Components/Utils/Navbar'
+import { Sidebar } from './Components/Utils/Sidebar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+    </>
+  )
+}
+
+export default App
